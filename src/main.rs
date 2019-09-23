c92274f9e6345326936bb2443817fdfe6ad59bfb
tingfei <1228959039@qq.com>
2019-09-23 10:59:05 +0800

    add link_list data structure

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 // use algo::quicksort::quicksort_lomuto;
 // use algo::array;
-use algo::ListNode;
+use algo::my_link_list::List;
 
 fn main() {
-    let node_1 = ListNode::new(1);
-    println!("{:?}", node_1);
+    let mut my_list = List::<u8>::new();
+    my_list.push(0);
+    my_list.push(1);
+    my_list.push(2);
+    println!("{:?}", my_list.len());
+    println!("{:?}", my_list.get_last_value());
+    my_list.pop();
+    println!("{:?}", my_list);
 }
