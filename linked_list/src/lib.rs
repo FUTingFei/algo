affd5f91c86e2af77a81787975ccea694402adb0
tingfei <1228959039@qq.com>
2019-10-11 23:01:07 +0800

    有个考试很忙，先开个坑

---
+++ linked_list/src/lib.rs
@@ -0,0 +1,13 @@
+// pub use leetcode_prelude::TreeNode;
+// pub use leetcode_prelude::btree;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        
+    }
+}
