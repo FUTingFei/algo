63ddbe6e2dbcab764775a88eefbd95413055d892
tingfei <1228959039@qq.com>
2019-10-24 22:13:02 +0800

    add is_power_of_three and count_primes

--- math/src/main.rs
+++ math/src/main.rs
@@ -1,7 +1,63 @@
 fn main() {
-    let vec = fizz_buzz(15);
-    println!("{:?}", vec);
+    let res = is_power_of_three(9);
+    println!("{:?}", res);
 }
+
+
+pub fn is_power_of_three(n: i32) -> bool {
+    let mut res = false;
+
+    if n == 1 {
+        res = true;
+    }
+
+    if n > 3 {
+        res = is_three(n);
+    }
+    
+    res
+}
+
+fn is_three(n: i32) -> bool {
+    let mut res = true;
+
+    if n == 3 {
+        return true;
+    }
+
+    if n%3 != 0 {
+        res = false;
+    } else {
+        res = is_three(n%3);
+    }
+
+    res
+}
+
+pub fn count_primes(n: i32) -> i32 {
+    let mut vec_bool: Vec<bool> = vec![true; n as usize];
+
+    for i in 2..n {
+        if vec_bool[i as usize] {
+            let mut j = i * 2;
+            while j < n {
+                vec_bool[j as usize] = false;
+                j = j + i;
+            }
+        }
+    }
+
+    let mut count = 0;
+    
+    for i in 2..n {
+        if vec_bool[i as usize] {
+            count += 1;
+        }
+    }
+
+    count
+}
+
 
 pub fn fizz_buzz(n: i32) -> Vec<String> {
     let mut v = Vec::new();
@@ -15,9 +71,11 @@ pub fn fizz_buzz(n: i32) -> Vec<String> {
             (true, true)  => "FizzBuzz".to_owned(),
             (false, false)=> i.to_string(),
         };
-        
+
         v.push(a);
     }
 
     v
 }
+
+
