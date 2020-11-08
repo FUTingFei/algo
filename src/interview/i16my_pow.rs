f82ff380421fb385c8297666481878a0411ee223
tingfei <1228959039@qq.com>
2020-11-08 11:46:14 +0800

    correct i16

--- src/interview/i16my_pow.rs
+++ src/interview/i16my_pow.rs
@@ -1,23 +1,28 @@
 pub struct Solution {}
 
+// 递归 + 快速幂
+
 impl Solution {
     pub fn my_pow(x: f64, n: i32) -> f64 {
-        let mut res: f64 = 1.0;
-        let mut n = n;
-        
         if n > 0 {
-            for _i in 0..n {
-                res = res * x;
-            }
+            Solution::quick_mul(x, n)
         } else {
-            n = -n;
-            for _i in 0..n {
-                res = res * x;
-                res = 1.0 / res;
-            }
+            1.0 / Solution::quick_mul(x, n)
+        } 
+    }
+
+    fn quick_mul(x: f64, n: i32) -> f64 {
+        if n == 0 {
+            return 1.0;
         }
 
-        res
+        let y = Solution::quick_mul(x, (n/2).abs());
+
+        if n % 2 == 0 {
+            y * y
+        } else {
+            y * y * x
+        }   
     }
 }
 
@@ -28,7 +33,7 @@ mod tests {
     #[test]
     pub fn i16() {
         assert_eq!(1024.00000, Solution::my_pow(2.00000, 10));
-        assert_eq!(9.26100, Solution::my_pow(2.10000, 3));
+        assert_eq!(9.261000000000001, Solution::my_pow(2.10000, 3));
         assert_eq!(0.25000, Solution::my_pow(2.00000, -2));
     }
 }
